e6c414d41357e97c26319e5249145328750b4dc9
Evan Varozza <VarozzaEJ@gmail.com>
2024-06-22 03:38:07 -0600

    Finished Functinality, added more styling.

--- app/models/Notes.js
+++ app/models/Notes.js
@@ -7,7 +7,7 @@ export class Note {
         this.id = generateId()
         this.name = data.name
         this.color = data.color
-        this.body = data.body
+        this.body = data.body == undefined ? this.body = '' : data.body
         this.count = 1
         this.dateListed = data.dateListed == undefined ? new Date() : new Date(data.dateListed)
         this.lastUpdatedAt = data.lastUpdatedAt ? new Date(data.lastUpdatedAt) : new Date()
@@ -29,8 +29,8 @@ export class Note {
     get ListTemplate() {
         return `
         
-        <div class=" border-bottom border-dark selectable ms-3" onclick="app.NotesController.setActiveNote('${this.id}')">
-            <h4 class="">${this.name}</h4>
+        <div class=" text-light border-bottom border-dark selectable py-1 ms-3" onclick="app.NotesController.setActiveNote('${this.id}')">
+            <h4 class="ms-2 ">${this.name}<i class="mdi mdi-circle ms-3 text-${this.color}"></i></h4>
         </div>
         `
     }
@@ -43,13 +43,15 @@ export class Note {
 
     get activeNoteTemplate() {
         return `
-        <div class=" d-flex">
+        <div class=" d-flex mx-4">
           <div class="col-4 mt-3">
-          <div class="d-flex align-items-center mb-2">
+          <div class="border border-dark rounded shadow bg-dark bg-gradient">
+          <div class="d-flex align-items-center justify-content-center mb-2">
             <h1 class="text-${this.color}">${this.name}</h1>
+          </div class="shadow border border-dark">
+          <h3 class="mb-3 mx-3 text-light fw-bold"><span class="d-flex text-${this.color}">Created On:</span> ${this.LongDate} </h3>
+          <h3 class="mb-3 mx-3 text-light fw-bold"><span class="d-flex text-${this.color}">Last Updated On:</span> ${this.LongOpenedOnDate} </h3>
           </div>
-          <h3 class="mb-3 fw-bold">Created On: ${this.LongDate} </h3>
-          <h3 class="mb-3 fw-bold">Last Updated On: ${this.LongOpenedOnDate} </h3>
           <div class="row mt-5">
             <div class="col-6 text-start mt-5">
               <button class="btn btn-danger rounded-pill fs-5 mt-5" onclick="app.NotesController.destroyNote('${this.id}')"><i class="mdi mdi-trash-can"></i></button>
@@ -57,13 +59,13 @@ export class Note {
           </div>
 
         </div>
-        <div class="col-8 d-flex bg-${this.color} justify-content-center flex-column mt-3 text-center">
+        <div class="col-8 ms-5 d-flex bg-${this.color} justify-content-center flex-column mt-3 text-center">
         <form class="activeNoteBody" onsubmit="app.NotesController.saveActiveNote()">
           <textarea name="activeNoteBody" id="activeNoteBody"
-            class="activeNoteTextArea">${this.body}</textarea>
+            class="activeNoteTextArea mt-5">${this.body}</textarea>
           <div class="row">
             <div class="col-12 d-flex justify-content-end mt-3">
-              <button type="submit" class="btn btn-info rounded-pill fs-5"><i class="mdi mdi-content-save"></i></button>
+              <button type="submit" class="btn btn-dark rounded-pill fs-5 me-3 mb-3"><i class="mdi mdi-content-save"></i></button>
             </div>
           </div>
           </form>
